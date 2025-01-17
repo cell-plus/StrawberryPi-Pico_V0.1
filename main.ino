0f92902b34e51c43480aae81c13a41fa0da041c6
cell-plus <tk.tasuku.plus9@gmail.com>
2025-01-17 22:45:01 +0900

    Update main.ino

--- main.ino
+++ main.ino
@@ -26,13 +26,13 @@
 #define SENSORPIN A0
 #define TRANSISTORPIN 23
 
-#define SENSOR_THRESHOLD 600     // センサーの閾値
-#define COUNT_THRESHOLD 12      // センサー閾値以下の回数の閾値
+#define SENSOR_THRESHOLD 600    // センサーの閾値
+#define COUNT_THRESHOLD 12      // センサー閾値以下の回数の閾値 12
 #define SENSOR_INTERVAL 5000    // センサー読み取りの間隔（ミリ秒）
-#define PUMP_ON_DURATION 30000  // ポンプをONにする時間（ミリ秒）
+#define PUMP_ON_DURATION 60000  // ポンプをONにする時間（ミリ秒）60000
 
-#define BLUE_THRESHOLD 350   // 青色にする閾値
-#define GREEN_THRESHOLD 450  // 緑色にする閾値
+#define BLUE_THRESHOLD 350    // 青色にする閾値
+#define GREEN_THRESHOLD 450   // 緑色にする閾値
 #define YELLOW_THRESHOLD 600  // 黄色にする閾値
 
 Adafruit_NeoPixel pixel0(1, ONBOARD_NP_PIN, NEO_GRB + NEO_KHZ800);
@@ -85,6 +85,12 @@ void checkPump(int sensorValue) {
     }
   }
 
+  if (pumpState) {
+    pixel0.setPixelColor(0, pixel0.Color(50, 50, 50));
+    pixel0.show();
+    delay(100);
+  }
+
   // ポンプステータスがONかつポンプをONにする時間に達した場合、ポンプOFF
   if (pumpState && currentMillis - pumpOnTime > PUMP_ON_DURATION) {
     pumpState = false;
@@ -98,7 +104,7 @@ void checkPump(int sensorValue) {
 
 void loop() {
   int sensorValue = analogRead(SENSORPIN);  // 土壌センサーの値を取得
-  checkPump(sensorValue);                       // ポンプの動作判定
+  checkPump(sensorValue);                   // ポンプの動作判定
 
   pixel0.setPixelColor(0, pixel0.Color(0, 0, 0));
   for (int i = 0; i < NUMPIXELS; i++) {
